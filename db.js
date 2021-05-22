96786215cf78690f30cf0cd201ab1c93f4e975d2
akool <akoolny@gmail.com>
2021-05-22 23:14:29 +0300

    fix: add Sequelize options 'port' and 'operatorsAliases'

--- db.js
+++ db.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    port: 5433,
+    operatorsAliases: false,
 })
 
 sequelize.authenticate().then(

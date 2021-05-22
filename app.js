f61a7890b91d838de904e22cd3ce380a7c313d92
akool <akoolny@gmail.com>
2021-05-23 00:51:06 +0300

    fix: set PORT for the app.listen

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(require('body-parser'));
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(function() {
+app.listen(process.env.PORT, function() {
     console.log("App is listening on 4000");
 })

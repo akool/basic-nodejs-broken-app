ea4fa8a07946047e4c2a0be9cc727fd69cd7f2be
akool <akoolny@gmail.com>
2021-05-23 12:01:05 +0300

    refactor: app.js: use ES6 syntax, set comma, set indentations

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var app = express();
-var db = require('./db');
-var user = require('./controllers/usercontroller');
-var game = require('./controllers/gamecontroller')
-
+const express = require('express');
+const app = express();
+const db = require('./db');
+const user = require('./controllers/usercontroller');
+const game = require('./controllers/gamecontroller');
 
 db.sync();
+
 app.use(express.json());
 app.use('/api/auth', user);
-app.use(require('./middleware/validate-session'))
+app.use(require('./middleware/validate-session'));
 app.use('/api/game', game);
-app.listen(process.env.PORT, function() {
-    console.log("App is listening on 4000");
-})
+app.listen(process.env.PORT, () => {
+  console.log(`App is listening on ${process.env.PORT}`);
+});

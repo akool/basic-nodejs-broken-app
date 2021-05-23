872d716360f41046715ad16f5520720e38f26524
akool <akoolny@gmail.com>
2021-05-23 11:56:54 +0300

    refactor: combing the sequelize connect

--- db.js
+++ db.js
@@ -2,20 +2,19 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    operatorsAliases: false,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
+  operatorsAliases: false,
 })
 
-sequelize.authenticate().then(
-    function success() {
-        console.log("Connected to DB");
-    },
-
-    function fail(err) {
-        console.log(`Error: ${err}`);
-    }
-)
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(`Error: ${err}`);
+  })
 
 module.exports = sequelize;
